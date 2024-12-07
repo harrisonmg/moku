cfd078add709a290515b6be92b2941a07d40e950
harrisonmg <gieraltowski.har@gmail.com>
2024-12-06 16:44:26 -0800

    stash

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,19 @@
 
 mod output;
 
+use enumset::EnumSet;
 pub use enumset::EnumSetType;
 
-pub trait StateList: Copy + EnumSetType {}
+pub trait StateEnum: Copy + EnumSetType {
+    fn get_decendents(&mut self) -> EnumSet<Self>;
+}
 
-pub enum StateEntry<T: StateList, U: State<T>> {
+pub enum StateEntry<T: StateEnum, U: State<T>> {
     State(U),
     Transition(T),
 }
 
-pub trait State<T: StateList>: Sized {
+pub trait State<T: StateEnum>: Sized {
     fn enter() -> StateEntry<T, Self>;
 
     fn update(&mut self) -> Option<T> {
@@ -23,55 +26,73 @@ pub trait State<T: StateList>: Sized {
     }
 }
 
-pub trait BranchState<T: StateList>: State<T> {
+pub trait BranchState<T: StateEnum>: State<T> {
     fn init(&mut self) -> T;
 }
 
 pub mod internal {
+    use std::marker::PhantomData;
+
     use enumset::EnumSet;
 
     use super::*;
 
-    pub trait Leaf<T: StateList, U: State<T>> {
-        const STATE: EnumSet<T>;
-
-        fn get_state(&mut self) -> &mut U;
-
-        fn take_state(self) -> U;
-
-        fn transition(&mut self, target: T) -> TransitionResult<T> {
-            if Self::STATE.contains(target) {
-                TransitionResult::Done
-            } else {
-                TransitionResult::MoveUp
-            }
-        }
-    }
-
-    pub trait Branch<T: StateList, U: BranchState<T>> {
-        const STATE: EnumSet<T>;
-        const CHILDREN: EnumSet<T>;
-
-        fn get_state(&mut self) -> &mut U;
-
-        fn take_state(self) -> U;
-
-        fn transition_child(&mut self, target: T) -> TransitionResult<T>;
-
-        fn transition(&mut self, target: T) -> TransitionResult<T> {
-            if Self::STATE.contains(target) {
-                TransitionResult::NewTransition(self.get_state().init())
-            } else if Self::CHILDREN.contains(target) {
-                self.transition_child(target)
-            } else {
-                TransitionResult::MoveUp
-            }
-        }
-    }
-
-    pub enum TransitionResult<T: StateList> {
+    pub enum TransitionResult<T: StateEnum> {
         Done,
         MoveUp,
         NewTransition(T),
     }
+
+    pub trait SubStateEnum<T: StateEnum, U: State<T>> {}
+
+    pub enum NodeEntry<T: StateEnum, U: State<T>, V: Node<T, U>> {
+        Node(V, PhantomData<U>),
+        Transition(T),
+    }
+
+    pub struct Node<T: StateEnum, U: State<T>, V: SubStateEnum<T, U>> {
+        state: U,
+        substate: V,
+        phantom: PhantomData<T>,
+
+        fn enter() -> NodeEntry<T, U, Self> {
+            match U::enter() {
+                StateEntry::State(state) => NodeEntry::Node(Self::from_state(state), PhantomData),
+                StateEntry::Transition(target) => NodeEntry::Transition(target),
+            }
+        }
+
+        fn update(&mut self) -> Option<T> {
+            self.state.update()
+        }
+    }
+
+    //pub trait Leaf<T: StateEnum, U: State<T>>: Node<T, U> {
+    //    const STATE: EnumSet<T>;
+
+    //    fn transition(&mut self, target: T) -> TransitionResult<T> {
+    //        if Self::STATE.contains(target) {
+    //            TransitionResult::Done
+    //        } else {
+    //            TransitionResult::MoveUp
+    //        }
+    //    }
+    //}
+
+    //pub trait Branch<T: StateEnum, U: BranchState<T>>: Node<T, U> {
+    //    const STATE: EnumSet<T>;
+    //    const CHILDREN: EnumSet<T>;
+
+    //    fn transition_child(&mut self, target: T) -> TransitionResult<T>;
+
+    //    fn transition(&mut self, target: T) -> TransitionResult<T> {
+    //        if Self::STATE.contains(target) {
+    //            TransitionResult::NewTransition(self.get_state().init())
+    //        } else if Self::CHILDREN.contains(target) {
+    //            self.transition_child(target)
+    //        } else {
+    //            TransitionResult::MoveUp
+    //        }
+    //    }
+    //}
 }
--- src/output.rs
+++ src/output.rs
@@ -1,200 +1,199 @@
-use crate::internal::*;
-use crate::*;
+//use crate::internal::*;
+//use crate::*;
 
-struct Top;
-impl State<BlinkyState> for Top {
-    fn enter() -> StateEntry<BlinkyState, Self> {
-        StateEntry::State(Self {})
-    }
-}
-impl BranchState<BlinkyState> for Top {
-    fn init(&mut self) -> BlinkyState {
-        BlinkyState::Enabled
-    }
-}
+//struct Top;
+//impl State<BlinkyState> for Top {
+//    fn enter() -> StateEntry<BlinkyState, Self> {
+//        StateEntry::State(Self {})
+//    }
+//}
+//impl BranchState<BlinkyState> for Top {
+//    fn init(&mut self) -> BlinkyState {
+//        BlinkyState::Enabled
+//    }
+//}
 
-struct Disabled;
-impl State<BlinkyState> for Disabled {
-    fn enter() -> StateEntry<BlinkyState, Self> {
-        StateEntry::State(Self {})
-    }
-}
+//struct Disabled;
+//impl State<BlinkyState> for Disabled {
+//    fn enter() -> StateEntry<BlinkyState, Self> {
+//        StateEntry::State(Self {})
+//    }
+//}
 
-struct Enabled;
-impl State<BlinkyState> for Enabled {
-    fn enter() -> StateEntry<BlinkyState, Self> {
-        StateEntry::State(Self {})
-    }
-}
-impl BranchState<BlinkyState> for Enabled {
-    fn init(&mut self) -> BlinkyState {
-        BlinkyState::LedOn
-    }
-}
+//struct Enabled;
+//impl State<BlinkyState> for Enabled {
+//    fn enter() -> StateEntry<BlinkyState, Self> {
+//        StateEntry::State(Self {})
+//    }
+//}
+//impl BranchState<BlinkyState> for Enabled {
+//    fn init(&mut self) -> BlinkyState {
+//        BlinkyState::LedOn
+//    }
+//}
 
-struct LedOn;
-impl State<BlinkyState> for LedOn {
-    fn enter() -> StateEntry<BlinkyState, Self> {
-        StateEntry::State(Self {})
-    }
-}
+//struct LedOn;
+//impl State<BlinkyState> for LedOn {
+//    fn enter() -> StateEntry<BlinkyState, Self> {
+//        StateEntry::State(Self {})
+//    }
+//}
 
-struct LedOff;
-impl State<BlinkyState> for LedOff {
-    fn enter() -> StateEntry<BlinkyState, Self> {
-        StateEntry::State(Self {})
-    }
-}
+//struct LedOff;
+//impl State<BlinkyState> for LedOff {
+//    fn enter() -> StateEntry<BlinkyState, Self> {
+//        StateEntry::State(Self {})
+//    }
+//}
 
-/// AUTOGEN
+///// AUTOGEN
 
-#[derive(Debug, EnumSetType)]
-enum BlinkyState {
-    Top,
-    Disabled,
-    Enabled,
-    LedOn,
-    LedOff,
-}
-impl StateList for BlinkyState {}
+//#[derive(Debug, EnumSetType)]
+//enum BlinkyState {
+//    Top,
+//    Disabled,
+//    Enabled,
+//    LedOn,
+//    LedOff,
+//}
+//impl StateEnum for BlinkyState {}
 
-mod moku_machine {
-    use crate::internal::*;
-    use enumset::{enum_set, EnumSet};
+//mod moku_machine {
+//    use crate::internal::*;
+//    use enumset::{enum_set, EnumSet};
 
-    use super::{BlinkyState as SL, State};
+//    use super::{BlinkyState as SE, State};
 
-    struct LedOn {
-        state: super::LedOn,
-    }
+//    struct LedOn {
+//        state: super::LedOn,
+//    }
 
-    impl Leaf<SL, super::LedOn> for LedOn {
-        const STATE: EnumSet<SL> = enum_set!(SL::LedOn);
+//    impl Leaf<SE, super::LedOn> for LedOn {
+//        const STATE: EnumSet<SE> = enum_set!(SE::LedOn);
 
-        fn get_state(&mut self) -> &mut super::LedOn {
-            &mut self.state
-        }
+//        fn get_state(&mut self) -> &mut super::LedOn {
+//            &mut self.state
+//        }
 
-        fn take_state(self) -> super::LedOn {
-            self.state
-        }
-    }
+//        fn take_state(self) -> super::LedOn {
+//            self.state
+//        }
+//    }
 
-    struct LedOff {
-        state: super::LedOff,
-    }
+//    struct LedOff {
+//        state: super::LedOff,
+//    }
 
-    impl Leaf<SL, super::LedOff> for LedOff {
-        const STATE: EnumSet<SL> = enum_set!(SL::LedOff);
+//    impl Leaf<SE, super::LedOff> for LedOff {
+//        const STATE: EnumSet<SE> = enum_set!(SE::LedOff);
 
-        fn get_state(&mut self) -> &mut super::LedOff {
-            &mut self.state
-        }
+//        fn get_state(&mut self) -> &mut super::LedOff {
+//            &mut self.state
+//        }
 
-        fn take_state(self) -> super::LedOff {
-            self.state
-        }
-    }
+//        fn take_state(self) -> super::LedOff {
+//            self.state
+//        }
+//    }
 
-    struct Enabled {
-        state: super::Enabled,
-        child: EnabledSubstate,
-    }
+//    struct Enabled {
+//        state: super::Enabled,
+//        child: EnabledSubstate,
+//    }
 
-    enum EnabledSubstate {
-        None,
-        LedOn(LedOn),
-        LedOff(LedOff),
-    }
+//    enum EnabledSubstate {
+//        None,
+//        LedOn(LedOn),
+//        LedOff(LedOff),
+//    }
 
-    impl Branch<SL, super::Enabled> for Enabled {
-        const STATE: EnumSet<SL> = enum_set!(SL::Enabled);
-        const CHILDREN: EnumSet<SL> = enum_set!(SL::LedOn | SL::LedOff);
+//    impl Branch<SE, super::Enabled> for Enabled {
+//        const STATE: EnumSet<SE> = enum_set!(SE::Enabled);
+//        const CHILDREN: [EnumSet<SE>; 2] = [enum_set!(SE::LedOn), enum_set!(SE::LedOff)];
 
-        fn get_state(&mut self) -> &mut super::Enabled {
-            &mut self.state
-        }
+//        fn get_state(&mut self) -> &mut super::Enabled {
+//            &mut self.state
+//        }
 
-        fn take_state(self) -> super::Enabled {
-            self.state
-        }
+//        fn take_state(self) -> super::Enabled {
+//            self.state
+//        }
 
-        fn transition_child(&mut self, target: SL) -> TransitionResult<SL> {
-            todo!();
-        }
-    }
+//        fn transition_child(&mut self, target: SE) -> TransitionResult<SE> {
+//            todo!();
+//        }
+//    }
 
-    struct Disabled {
-        state: super::Disabled,
-    }
+//    struct Disabled {
+//        state: super::Disabled,
+//    }
 
-    impl Leaf<SL, super::Disabled> for Disabled {
-        const STATE: EnumSet<SL> = enum_set!(SL::Disabled);
+//    impl Leaf<SE, super::Disabled> for Disabled {
+//        const STATE: EnumSet<SE> = enum_set!(SE::Disabled);
 
-        fn get_state(&mut self) -> &mut super::Disabled {
-            &mut self.state
-        }
+//        fn get_state(&mut self) -> &mut super::Disabled {
+//            &mut self.state
+//        }
 
-        fn take_state(self) -> super::Disabled {
-            self.state
-        }
-    }
+//        fn take_state(self) -> super::Disabled {
+//            self.state
+//        }
+//    }
 
-    struct Top {
-        state: super::Top,
-        child: TopSubstate,
-    }
+//    struct Top {
+//        state: super::Top,
+//        child: TopSubstate,
+//    }
 
-    enum TopSubstate {
-        None,
-        Disabled(Disabled),
-        Enabled(Enabled),
-    }
+//    enum TopSubstate {
+//        None,
+//        Disabled(Disabled),
+//        Enabled(Enabled),
+//    }
 
-    impl Branch<SL, super::Top> for Top {
-        const STATE: EnumSet<SL> = enum_set!(SL::Top);
-        const CHILDREN: EnumSet<SL> = enum_set!(SL::Disabled | SL::Enabled);
+//    impl Branch<SE, super::Top> for Top {
+//        const STATE: EnumSet<SE> = enum_set!(SE::Top);
+//        const CHILDREN: EnumSet<SE> = enum_set!(SE::Disabled | SE::Enabled);
 
-        fn get_state(&mut self) -> &mut super::Top {
-            &mut self.state
-        }
+//        fn get_state(&mut self) -> &mut super::Top {
+//            &mut self.state
+//        }
 
-        fn take_state(self) -> super::Top {
-            self.state
-        }
+//        fn take_state(self) -> super::Top {
+//            self.state
+//        }
 
-        fn transition_child(&mut self, target: SL) -> TransitionResult<SL> {
-            if !matches!(self.child, TopSubstate::None) {
-                let transition_res = match self.child {
-                    TopSubstate::Disabled(ref mut substate) => substate.transition(target),
-                    TopSubstate::Enabled(ref mut substate) => substate.transition(target),
-                    TopSubstate::None => unreachable!(),
-                };
+//        fn transition_child(&mut self, target: SE) -> TransitionResult<SE> {
+//            if !matches!(self.child, TopSubstate::None) {
+//                let transition_res = match self.child {
+//                    TopSubstate::Disabled(ref mut substate) => substate.transition(target),
+//                    TopSubstate::Enabled(ref mut substate) => substate.transition(target),
+//                    TopSubstate::None => unreachable!(),
+//                };
 
-                match transition_res {
-                    TransitionResult::Done => return TransitionResult::Done,
-                    TransitionResult::NewTransition(new_target) => {
-                        return self.transition(new_target)
-                    }
-                    TransitionResult::MoveUp => {
-                        let child = std::mem::replace(&mut self.child, TopSubstate::None);
-                        let exit_res = match child {
-                            TopSubstate::Disabled(substate) => substate.take_state().exit(),
-                            TopSubstate::Enabled(substate) => substate.take_state().exit(),
-                            TopSubstate::None => unreachable!(),
-                        };
+//                match transition_res {
+//                    TransitionResult::Done => return TransitionResult::Done,
+//                    TransitionResult::NewTransition(new_target) => self.transition(new_target),
+//                    TransitionResult::MoveUp => {
+//                        let child = std::mem::replace(&mut self.child, TopSubstate::None);
+//                        let exit_res = match child {
+//                            TopSubstate::Disabled(substate) => substate.take_state().exit(),
+//                            TopSubstate::Enabled(substate) => substate.take_state().exit(),
+//                            TopSubstate::None => unreachable!(),
+//                        };
 
-                        match exit_res {
-                            Some(new_target) => self.transition(new_target),
-                            None => TransitionResult::MoveUp,
-                        }
-                    }
-                }
-            } else {
-                match target {
-                    SL::
-                }
-            }
-        }
-    }
-}
+//                        match exit_res {
+//                            Some(new_target) => self.transition(new_target),
+//                            None => TransitionResult::MoveUp,
+//                        }
+//                    }
+//                }
+//            } else {
+//                todo!()
+//                //match target {
+//                //    SE::
+//                //}
+//            }
+//        }
+//    }
+//}
